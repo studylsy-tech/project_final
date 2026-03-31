67140cd61f4ac64147fc7c73028a7a486f42f7ea
test_git <studylsy@gmail.com>
2026-03-31 15:59:36 +0900

    merge오류 수정

--- src/main/java/com/project/dao/BoardMapper.java
+++ src/main/java/com/project/dao/BoardMapper.java
@@ -1,19 +1,9 @@
 package com.project.dao;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-<<<<<<< HEAD
-import org.apache.ibatis.annotations.Param;
-=======
-
->>>>>>> branch 'develop' of https://github.com/studylsy-tech/project_final.git
 import com.project.model.BoardDTO;
-<<<<<<< HEAD
-import com.project.util.Criteria; 
-=======
-import com.project.util.SearchCriteria;
->>>>>>> branch 'develop' of https://github.com/studylsy-tech/project_final.git
+import com.project.util.SearchCriteria; // Using the more robust criteria object
 
 @Mapper
 public interface BoardMapper {
@@ -25,25 +15,14 @@ public interface BoardMapper {
     void insertBoard(BoardDTO board);
     
     void updateCount(int notice_no);
-<<<<<<< HEAD
+    
+    // Using the SearchCriteria object for cleaner pagination and searching
+    List<BoardDTO> selectBoardListPaging(SearchCriteria scri);
+    
+    int getBoardCount(SearchCriteria scri);
 
-    int getBoardCount(String boardType);
+	List<BoardDTO> selectBoardListPaging(String boardType, int pageStart, int pageEnd, String searchType,
+			String keyword);
 
-    // MyBatis에서 인자가 2개 이상일 때는 @Param 어노테이션을 붙여주는 것이 안전합니다.
-    List<BoardDTO> selectBoardListPaging(
-    	    @Param("boardType") String boardType, 
-    	    @Param("pageStart") int pageStart, 
-    	    @Param("pageEnd") int pageEnd,
-    	    @Param("searchType") String searchType, // 추가
-    	    @Param("keyword") String keyword       // 추가
-    	);
-=======
-    
-	 
-    // 검색 및 페이징 처리
-	 List<BoardDTO> selectBoardListPaging(SearchCriteria scri);
-	 int getBoardCount(SearchCriteria scri);
-    
-    
->>>>>>> branch 'develop' of https://github.com/studylsy-tech/project_final.git
+	int getBoardCount(String boardType);
 }
