a8d41fe0503a5cfed8560a562c9a29948dc5f8a2
test_git <ovg1210@gmail.com>
2026-04-10 17:42:51 +0900

    회원가입 시 전화번호 인증 구현 완료

--- src/main/java/com/project/service/MemberService.java
+++ src/main/java/com/project/service/MemberService.java
@@ -2,6 +2,7 @@ package com.project.service;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,11 +12,17 @@ import org.springframework.stereotype.Service;
 import com.project.dao.MemberMapper;
 import com.project.model.MemberDTO;
 
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.service.DefaultMessageService;
 @Service
 public class MemberService {
 
 	@Autowired
 	private MemberMapper memberMapper;
+	
+	private DefaultMessageService messageService; // coolsms 휴대폰 인증
 	
 
 	// 회원 가입
@@ -82,6 +89,31 @@ public class MemberService {
 	// 비밀번호 찾기 시 회원 존재 여부 확인 (필요하다면 추가)
 	public int checkMemberForPw(MemberDTO member) {
 	    return memberMapper.checkUserForPw(member);
+	}
+	
+	@PostConstruct
+	public void init() {
+		// 발급받으신 실제 키를 적용했습니다.
+		this.messageService = NurigoApp.INSTANCE.initialize(
+            "NCSBDVPFHB4RXIPQ", 
+            "S2JASPGD6D3COYUIWCVLUUROSLCMM6J5", 
+            "https://api.coolsms.co.kr"
+        );
+	}
+
+	public void sendSms(String phone, String code) {
+		Message message = new Message();
+		// [수정 필요] CoolSMS 사이트에 등록한 본인 휴대폰 번호를 숫자만 입력하세요.
+		message.setFrom("01095662234"); 
+		message.setTo(phone);
+		message.setText("[득템] 인증번호는 [" + code + "] 입니다.");
+
+		try {
+			this.messageService.sendOne(new SingleMessageSendingRequest(message));
+		} catch (Exception e) {
+			e.printStackTrace();
+			// 에러 발생 시 로그를 남기거나 예외 처리를 추가할 수 있습니다.
+		}
 	}
 	
 }
